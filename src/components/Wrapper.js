c961ceeb4b919c4bfd2534104a16ddbda7153a10
Thomas Wilson <twilson.spqr@gmail.com>
2017-10-06 14:50:55 -0400

    first commit

---
+++ src/components/Wrapper.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Wrapper = props => {
+  return (
+    <div className='box wrapper'>
+      <h1>I Am the Wrapper</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sed
+        ullamcorper nibh, id efficitur eros. Suspendisse ultricies est ut mi
+        volutpat, quis faucibus sem malesuada. Pellentesque pellentesque ex at
+        posuere viverra. Nunc maximus massa nec lectus malesuada sodales. Lorem
+        ipsum dolor sit amet, consectetur adipiscing elit. Cras eget malesuada
+        tortor.
+      </p>
+    </div>
+  )
+}
+
+export default Wrapper;
